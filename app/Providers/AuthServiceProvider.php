18f54c544175d50f83ffb41865641c16d1be44f1
Ngo Quoc Thi <quocthingo@gmail.com>
2019-11-06 01:22:51 +0700

    authentication-authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('manage-users', function ($user){
+            return $user->hasAnyRoles(['admin','user']);
+        });
+        Gate::define('edit-users', function ($user){
+           return $user->hasRole('admin','user');
+        });
+        Gate::define('delete-users', function ($user){
+            return $user->hasRole('admin');
+        });
     }
 }
---
+++ database/migrations/2019_11_05_084030_create_user_roles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('id')
+                ->references('role_id')
+                ->on('roles')
+                ->onUpdate('cascade');
+
+            $table->foreign('id')
+                ->references('user_id')
+                ->on('users')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}
